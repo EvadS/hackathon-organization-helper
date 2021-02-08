bf9ef71a88be4d862249c44a11b86a4d2a2fc897
evgeniy_skiba <eugene.s@it-dimension.com>
2021-02-08 16:30:44 +0200

    Web and Jpa dependency. Index page

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # hackathon-organization-helper
 Development and integration of a web application client for participants and organizers of hackathons
+
+
+## Run depend on environment profile
+
+gradle wrapper
+
+* Run with maven plugin
+```bash
+    ./gradlew bootRun -Plocal
+```
+
+
+ ./gradlew bootRun --args='--spring.profiles.active=local'
+
+
+* Run as a java application
+```bash
+    java -jar -Dspring.profiles.active=dev ..?../admin-backend-api-1.0-SNAPSHOT.jar
+```
---
+++ src/main/java/com/se/pet/hackathonorganizationhelper/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.se.pet.hackathonorganizationhelper.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/app/**/*.{js,html}")
+                .antMatchers("/i18n/**")
+                .antMatchers("/content/**")
+                .antMatchers("/swagger-ui/index.html")
+                .antMatchers("/test/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .csrf()
+            .disable()
+            .exceptionHandling()
+        .and()
+            .authorizeRequests()
+            .antMatchers("/").permitAll();
+        // @formatter:on
+    }
+}
