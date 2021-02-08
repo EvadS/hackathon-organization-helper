92cdb14d237c30ca37fc9eaffe3206498b4a15ff
evgeniy_skiba <eugene.s@it-dimension.com>
2021-02-08 12:00:13 +0200

    initial commit

---
+++ src/main/java/com/se/pet/hackathonorganizationhelper/HackathonOrganizationHelperApplication.java
@@ -0,0 +1,13 @@
+package com.se.pet.hackathonorganizationhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonOrganizationHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonOrganizationHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se/pet/hackathonorganizationhelper/HackathonOrganizationHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.pet.hackathonorganizationhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonOrganizationHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
